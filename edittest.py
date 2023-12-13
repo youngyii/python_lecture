ba8a9f182a2a8ace486e06a9178851f24f78fd4b
youngyii <yishinyoung0708@gmail.com>
2023-12-13 22:39:14 +0900

    add edit test

---
+++ edittest.py
@@ -0,0 +1,26 @@
+a = "hello"
+b = "world"
+print(a, b)
+
+print(a)
+print(b)
+print(b, a)
+print("python", a, b)
+
+number_school = 2394584903          # 정수형 변수
+str_name = "python"                 # 문자열 변수
+print(number_school, str_name)
+
+bool_view = False                   # boolean 변수
+print(bool_view)
+
+flt_pi = 3.14                       # 실수형 변수
+print(flt_pi)
+
+char_element = 'a'                  # 문자형 변수
+print(char_element)
+
+chars = "hello world"
+print(chars)
+
+print("number : ", number_school, " name : ", str_name, " bool : ", bool_view, " float : ", flt_pi, " character : ", char_element, " characters : ", chars)
